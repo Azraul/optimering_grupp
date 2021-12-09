82b39b4cf1085549abf983c897300044f21597e0
stahlfre <stahlfre@arcada.fi>
2021-12-09 13:55:04 +0200

    print reslults in backtracking

--- backtracking.py
+++ backtracking.py
@@ -86,4 +86,12 @@ grid = [
 
 
 # sen grid/sudoku of choice into function
-solve(sudokus['expert'][4])
+#solve(sudokus['expert'][4])
+
+# Vi skriver allt till txt-file
+
+to_solve = [sudokus['easy'][2], sudokus['expert'][4]]
+
+for sudoku in to_solve:
+        print(f"{np.array(sudoku)}")
+        print(f"{solve(sudoku)}\n\n")
